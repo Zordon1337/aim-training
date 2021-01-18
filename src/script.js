e5a569bd5507f326311e2bca4e00f86412ea7273
sander <68357029+sander15@users.noreply.github.com>
2021-01-17 21:49:16 -0600

    Uploading the files

---
+++ src/script.js
@@ -0,0 +1,41 @@
+let points = 0;
+let PointsNeeded = 25;
+let seconds = 30;
+
+document.getElementById("time").textContent = seconds;
+
+const sumarPuntos = ()=>{
+	if (points >= PointsNeeded) {
+		alert("You Won!! You got the points needed.");
+		points = 0;
+		seconds = 30;
+    }
+	else points ++;
+	document.getElementById("points").textContent = points
+}
+
+const moverPersonaje = ()=>{
+	const circle = document.querySelector(".circle");
+	randomX = Math.random()*500;
+	randomY = Math.random()*500;
+	circle.style.top = `${randomY}px`;
+	circle.style.left = `${randomX}px`;
+}
+
+document.querySelector(".circle").addEventListener("mouseenter",()=>{
+	sumarPuntos();
+	moverPersonaje();
+});
+
+
+setInterval(()=>{
+	seconds--;
+	document.getElementById("time").textContent = seconds;
+	if (seconds == 0) {
+		alert("You Lost!! Time is over.");
+		points = 0;
+	    document.getElementById("points").textContent = points;
+		seconds = 30;
+	}
+	
+} ,1000)
