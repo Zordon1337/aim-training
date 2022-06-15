3ada8aa1257d237612dc41a2366783af02919098
Zordon <65111609+Zordon1337@users.noreply.github.com>
2022-06-15 23:49:00 +0200

    Test Release

---
+++ normal/src/script.js
@@ -0,0 +1,41 @@
+let points = 0;
+let PointsNeeded = 69;
+let seconds = 727;
+
+document.getElementById("time").textContent = seconds;
+
+const addingPoints = ()=>{
+	if (points >= PointsNeeded) {
+		alert("You Won!! You got the points needed.");
+		points = 0;
+		seconds = 60;
+    }
+	else points ++;
+	document.getElementById("points").textContent = points
+}
+
+const moveCharacter = ()=>{
+	const circle = document.querySelector(".circle");
+	randomX = Math.random()*800;
+	randomY = Math.random()*800;
+	circle.style.top = `${randomY}px`;
+	circle.style.left = `${randomX}px`;
+}
+
+document.querySelector(".circle").addEventListener("mouseenter",()=>{
+	addingPoints();
+	moveCharacter();
+});
+
+
+setInterval(()=>{
+	seconds--;
+	document.getElementById("time").textContent = seconds;
+	if (seconds == 0) {
+		alert("You Lost!! Time is over.");
+		points = 0;
+	    document.getElementById("points").textContent = points;
+		seconds = 60;
+	}
+	
+} ,1000)
